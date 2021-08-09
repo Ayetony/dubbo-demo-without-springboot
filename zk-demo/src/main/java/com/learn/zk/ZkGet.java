434642e984ef5f99d89852f73107ac317641aa9c
miao.ayetony <miao.ayetony@gmail.com>
2021-08-10 00:41:49 +0800

    get data async

--- zk-demo/src/main/java/com/learn/zk/ZkGet.java
+++ zk-demo/src/main/java/com/learn/zk/ZkGet.java
@@ -1,24 +1,64 @@
 package com.learn.zk;
 
+import org.apache.zookeeper.AsyncCallback;
 import org.apache.zookeeper.KeeperException;
 import org.apache.zookeeper.ZooKeeper;
 import org.apache.zookeeper.data.Stat;
 
 public class ZkGet {
 
-    public static Stat getData(String path ,Boolean watch) throws InterruptedException, KeeperException {
+    public static DataStat getData(String path ,Boolean watch) throws InterruptedException, KeeperException {
         ZooKeeper zooKeeper = ZKConn.getZkClientConnection();
         if(zooKeeper == null){
             throw new RuntimeException("zookeeper connect error");
         }
         Stat stat = new Stat();
-        zooKeeper.getData(path,watch,stat);
-        return stat;
+        byte[] bytes = zooKeeper.getData(path,watch,stat);
+        DataStat dataStat = new DataStat();
+        dataStat.setStat(stat);
+        dataStat.setData(new String(bytes));
+        return dataStat;
+    }
+
+    public static void asyncGetData(String path ,Boolean watch) throws InterruptedException, KeeperException {
+        ZooKeeper zooKeeper = ZKConn.getZkClientConnection();
+        if(zooKeeper == null){
+            throw new RuntimeException("zookeeper connect error");
+        }
+        zooKeeper.getData(path, watch, new AsyncCallback.DataCallback() {
+            @Override
+            public void processResult(int i, String s, Object o, byte[] bytes, Stat stat) {
+                System.out.println(new String(bytes));
+            }
+        },"async data callback context");
+    }
+
+    static class DataStat{
+
+        private Stat stat;
+        private String data;
+
+        public Stat getStat() {
+            return stat;
+        }
+
+        public void setStat(Stat stat) {
+            this.stat = stat;
+        }
+
+        public String getData() {
+            return data;
+        }
+
+        public void setData(String data) {
+            this.data = data;
+        }
     }
 
     public static void main(String[] args) throws InterruptedException, KeeperException {
-        Stat stat = getData("/hadoop/node12",false);
-        System.out.println("version : " + stat.getVersion() + ", data aversion : " + stat.getAversion());
+        asyncGetData("/hadoop/node12",false);
+        DataStat dataStat = getData("/hadoop/node12",false);
+        System.out.println("Data msg : " +  dataStat.getData()  + " version : " + dataStat.getStat().getVersion() + ", data aversion : " + dataStat.getStat().getAversion());
     }
 
 }
